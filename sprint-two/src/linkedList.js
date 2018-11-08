c4ff6bc78cc727a2dd5939bf4eff882bd3b2c122
meandeane <aaronmdeane@gmail.com>
2018-11-08 14:20:39 -0800

    Updated LinkedList with pseudocode.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,17 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // if list.head and list.tail are both null when addToTail is invoked
+      // create newNode
+      // list.head and list.tail both assigned newNode 
+    // var newNode = Node(4);
+    // node = {
+    //   value: 4,
+    //   next: null
+    // }
+    
+    // iterate thru keys in linkedList 
+      // 
   };
 
   list.removeHead = function() {
@@ -27,3 +38,8 @@ var Node = function(value) {
 /*hello
  * Complexity: What is the time complexity of the above functions?
  */
+
+// instantiate
+// var linkedList = LinkedList();
+
+// linkedList.addToTail(4); 
