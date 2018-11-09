894035c696fdde5aeee452b80d27f0898df54aa1
meandeane <aaronmdeane@gmail.com>
2018-11-08 21:30:36 -0800

    partial impl 2

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -39,6 +39,9 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  
+  // check if value exists using IndexOf 
+  // 
 };
 
 // Pass in a callback which will be executed on each node of the graph.
