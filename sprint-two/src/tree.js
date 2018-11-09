bc31b6f2564c6a10b335fed11cf489660a42b0a7
meandeane <aaronmdeane@gmail.com>
2018-11-08 16:27:21 -0800

    completed 4 tests in tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,11 +12,30 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // Create a var for new child and insantiate and instance(value)
+  var newChild = Tree(value);
+  // if this.children has no length
+  this.children.push(newChild);
+  
 };
 
 treeMethods.contains = function(target) {
+  // Check top level tree object for target value
+  if (this.value === target) {
+    return true;
+  }
+  
+  // If there are children, iterate through children
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      this.children[i].contains;
+    }
+    // call contains on the child object
+  }
+  //
+  return false;
+  
 };
-
 
 
 /*
