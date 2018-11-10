da145415d1e54913d954264e945a821648a8edb5
meandeane <aaronmdeane@gmail.com>
2018-11-09 19:06:28 -0800

    completing last test of hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Generate a hash index based on the key 'val1'
+  // Call retrieve on the key to return the array at that index
+  // push the value to the returned array
+  // pass the new array into set with the hased index and both values
+  
+  
+  this._storage.set(index, v);
 };
+  
+  
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  return this._storage.get(index);
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  this._storage.set(index, undefined); 
+  
 };
 
 
